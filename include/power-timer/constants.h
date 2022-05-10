69b6a9f95fa3b439150df2af37aca10a477dff7a
Sergii Zasenko <sergii@zasenko.name>
2022-05-10 08:38:22 +0300

    migrate to gitlab

--- include/power-timer/constants.h
+++ include/power-timer/constants.h
@@ -7,7 +7,7 @@
 #define TIMER_DEFAULT 45
 #define TIME_SECONDS 60
 
-#define WEBSITE  "http://zasenko.name"
+#define WEBSITE  "https://gitlab.xfce.org/szasenko/xfce4-powertimer-plugin"
 #define ICON_NAME "xfce4-power-timer"
 #define COPYRIGHT "Copyright \xc2\xa9 2022 Serhii Zasenko\n"
 
